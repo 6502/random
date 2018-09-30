8f36923d2c60a349730e28a246ef0093164f5c50
Andrea Griffini <agriff@tin.it>
2018-09-30 11:08:44 +0200

    fix multimodules & doc

--- random.h
+++ random.h
@@ -28,33 +28,46 @@ SOFTWARE.
 ******************************************************************************/
 
 #include <algorithm>
+#include <atomic>
 #include <chrono>
 
-thread_local unsigned random_state = unsigned(std::chrono::duration_cast< std::chrono::milliseconds >(
-                                                  std::chrono::system_clock::now().time_since_epoch()
-                                                  ).count());
-
-inline void rndNext() {
-    random_state ^= random_state << 13;
-    random_state ^= random_state >> 17;
-    random_state ^= random_state << 5;
+inline unsigned& random_state() {
+    static std::atomic<unsigned> x;
+    thread_local unsigned rs = (unsigned(std::chrono::duration_cast< std::chrono::milliseconds >(
+                                             std::chrono::system_clock::now().time_since_epoch()
+                                             ).count())
+                                + (x += 1500450271));
+    return rs;
 }
 
-inline unsigned rnd(int max) {
+inline void rndNext() {
+    unsigned& rs = random_state();
+    rs ^= rs << 13;
+    rs ^= rs >> 17;
+    rs ^= rs << 5;
+}
+
+inline int rnd(int max) {
+    unsigned& rs = random_state();
     if (max > (1<<20)) {
-        unsigned lim = (1ull<<32) / max * max;
+        unsigned lim = ((1ull<<32)-1) / max * max;
         for(;;) {
             rndNext();
-            if (random_state < lim) break;
+            if (rs < lim) break;
         }
     } else {
         rndNext();
     }
-    return random_state % max;
+    return rs % max;
 }
 
 inline double rnd() {
-    return rnd(1<<30) / double(1<<30);
+    unsigned& rs = random_state();
+    rndNext();
+    unsigned long long x = rs;
+    rndNext();
+    x |= ((unsigned long long)rs) << 32;
+    return x / 18446744073709551616.0;
 }
 
 template<typename RIT>
